4b672e744338d81f9bea49dd16337edf192f8278
Raffaello Damgaard <raffaello_neves_ext@carrefour.com>
2023-01-23 17:30:44 -0300

    Commit inicial.

---
+++ src/main/java/com/sharebook/pubsubpoc/PubsubpocApplication.java
@@ -0,0 +1,13 @@
+package com.sharebook.pubsubpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubsubpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubsubpocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharebook/pubsubpoc/PubsubpocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharebook.pubsubpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubsubpocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
